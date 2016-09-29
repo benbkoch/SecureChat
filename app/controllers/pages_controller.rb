5b54e8a083c1d0922a62ab72befab965bf01adfe
Ben Koch <benbkoch@gmail.com>
2016-09-29 16:15:29 -0700

    let's encrypt 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,5 @@ class PagesController < ApplicationController
 
   def letsencrypt
     # use your code here, not mine
-    render text: "QxJ1Dte4pZMWxkJ7skwL5cq8NHKN7u7SqywY5gZ4igM.wK-MSD4N7ka0-J4PXNYrq5pl413tO7hBGADa3tlrRhY"
+    render text: "dGxGmNYKAxrPq3GVURHdKiju-dLgkVB_e4QC4DSJyuo.wK-MSD4N7ka0-J4PXNYrq5pl413tO7hBGADa3tlrRhY"
 end

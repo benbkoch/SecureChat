d5b1bb1bf03b3eec8bc68825e5b6704f8d64424b
Ben Koch <benbkoch@gmail.com>
2016-09-29 16:10:24 -0700

    let's encrypt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def letsencrypt
+    # use your code here, not mine
+    render text: "QxJ1Dte4pZMWxkJ7skwL5cq8NHKN7u7SqywY5gZ4igM.wK-MSD4N7ka0-J4PXNYrq5pl413tO7hBGADa3tlrRhY"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

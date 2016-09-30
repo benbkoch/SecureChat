a7864aef83cadf4dbc50b8716062bd9e977b622d
Ben Koch <benbkoch@gmail.com>
2016-09-29 19:48:15 -0700

    update cert

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
 
   def letsencrypt
     # use your code here, not mine
-    render text: "RoVgyfs38gjZYuSDc40-PYGpe1SUYLmQsxMVo9VpDss.wK-MSD4N7ka0-J4PXNYrq5pl413tO7hBGADa3tlrRhY"
+    render text: "5O24PQPLYF6lExqC4C86jtWME8TDOWsNXOl1zD4t0_0.wK-MSD4N7ka0-J4PXNYrq5pl413tO7hBGADa3tlrRhY"
   end
 end

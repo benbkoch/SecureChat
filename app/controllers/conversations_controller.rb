7697d7ea851af959276db1238aee09d03f62947f
Ben Koch <benbkoch@gmail.com>
2016-10-06 20:37:12 -0700

    try to fix email

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,14 +3,16 @@ class ConversationsController < ApplicationController
 	def index
  		@names = User.pluck :email
  		@conversations = Conversation.all
- 		render json: @names
+ 		respond_to do |format|
+      		format.json{ render json: @names }
+      	end
  	end
 
  	def create
  		sender_id = current_user.id
  		recipient_id = User.where(email: params[:recipient_email])[0][:id]
 		 if Conversation.between(sender_id,receiver_id).present?
-		    @conversation = Conversation.between(sender_id, recipient_id).first
+		    @conversation = Conversation.between(sender_id, recipient_email_id).first
 		 else
 		  	@conversation = Conversation.create!(sender_id: sender_id, recipient_id: receiver_id)
 		 end

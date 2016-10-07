eb4bab9b59170cd88d0083d3b58dfe56aa5c5d37
Ben Koch <benbkoch@gmail.com>
2016-10-07 12:43:49 -0700

    error in migration

--- db/migrate/20161007185300_add_read_each_to_users.rb
+++ db/migrate/20161007185300_add_read_each_to_users.rb
@@ -1,7 +1,6 @@
 class AddReadEachToUsers < ActiveRecord::Migration[5.0]
   def change
   	remove_column :messages, :read, :boolean
-  	remove_column :users, :read, :boolean
     add_column :messages, :read_by_1, :boolean, default: false
     add_column :messages, :read_by_2, :boolean, default: false
   end

2805a29db8ab5c795716790bb23fa570c38d6fa6
Ben Koch <benbkoch@gmail.com>
2016-10-10 21:39:46 -0700

    require email authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :registerable
+          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 end

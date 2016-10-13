e3068ae8f1286cce899f5f6788b2b6656443c427
Ben Koch <benbkoch@gmail.com>
2016-10-13 11:44:43 -0700

    Make token last 3 days instead of 2 weeks

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -10,7 +10,7 @@ DeviseTokenAuth.setup do |config|
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
-  # config.token_lifespan = 2.weeks
+  config.token_lifespan = 3.days
 
   # Sets the max number of concurrent devices per user, which is 10 by default.
   # After this limit is reached, the oldest tokens will be removed.
